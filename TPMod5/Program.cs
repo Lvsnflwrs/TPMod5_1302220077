95625512b3e470e7d57e1fc5a6c3b956488da8a4
lvsnflwrs <149216118+Lvsnflwrs@users.noreply.github.com>
2024-03-18 00:47:28 +0700

    Benerin Code
    ada yang salah bang disini, jadi dibenerin disini nya aja

--- TPMod5/Program.cs
+++ TPMod5/Program.cs
@@ -1,47 +1,41 @@
-﻿using System;
+using System;
 
-<<<<<<< HEAD
+
 public class HaloGeneric
 {
     public void SapaUser<T>(T nama)
     {
         Console.WriteLine($"Halo user {nama}");
     }
-}
-=======
-public class DataGeneric<T>
-{
-    public T Data { get; set; }
 
-    public DataGeneric(T data)
+    public class DataGeneric<T>
     {
-        Data = data;
+        public T Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
     }
 
-    public void PrintData()
+
+    public class Program
     {
-        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        public static void Main(string[] args)
+        {
+
+            HaloGeneric sapa = new HaloGeneric();
+            string namaPanggilan = "Praktikan";
+            sapa.SapaUser(namaPanggilan);
+
+            string nim = "1302220077";
+            DataGeneric<string> data = new DataGeneric<string>(nim);
+            data.PrintData();
+        }
     }
 }
-
->>>>>>> generic-class
-public class Program
-{
-    public static void Main(string[] args)
-    {
-<<<<<<< HEAD
-        HaloGeneric sapa = new HaloGeneric();
-        string namaPanggilan = "Praktikan";
-        sapa.SapaUser(namaPanggilan);
-
-        string nim = "1302220077";
-    }
-}
-=======
-        string nim = "1302220077";
-
-        DataGeneric<string> data = new DataGeneric<string>(nim);
-        data.PrintData();
-    }
-}
->>>>>>> generic-class

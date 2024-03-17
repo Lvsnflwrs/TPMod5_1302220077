d4218482a466d7063afbed56f0d23a548bcc343f
lvsnflwrs <149216118+Lvsnflwrs@users.noreply.github.com>
2024-03-18 00:25:03 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TPMod5/Program.cs
+++ TPMod5/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine($"Halo user {nama}");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric sapa = new HaloGeneric();
+        string namaPanggilan = "Praktikan";
+        sapa.SapaUser(namaPanggilan);
+
+        string nim = "1302220077";
+    }
+}

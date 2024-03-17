2c825521dcafd57a5e72c73a85200f7b345943b7
lvsnflwrs <149216118+Lvsnflwrs@users.noreply.github.com>
2024-03-18 00:32:03 +0700

    menambahkan class DataGeneric

--- TPMod5/Program.cs
+++ TPMod5/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string nim = "1302220077";
+
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+}
